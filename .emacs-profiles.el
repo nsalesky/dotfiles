eace3471ce6236aca019651a636caaf1ed9f14bd
Nick Salesky <salesky.n@northeastern.edu>
2022-03-08 17:24:23 -0500

    Started creating a new custom Emacs configuration

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,5 @@
 (("doom" . ((user-emacs-directory . "~/emacs-configs/doom-emacs")
 	    ((env . (("DOOMDIR" . "~/.dotfiles/.doom.d"))))))
  ("spacemacs" . ((user-emacs-directory . "~/emacs-configs/spacemacs")
-		 (env . (("SPACEMACSDIR" . "~/.dotfiles/.spacemacs.d"))))))
+		 (env . (("SPACEMACSDIR" . "~/.dotfiles/.spacemacs.d")))))
+ ("custom" . ((user-emacs-directory . "~/.dotfiles/.emacs.d"))))
---
+++ .emacs.d/init.el
@@ -0,0 +1,62 @@
+;; Basic UI settings
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1)   ; Disable the toolbar
+(tooltip-mode -1)    ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
+(menu-bar-mode -1)   ; Disable the menu bar
+(setq ring-bell-function 'ignore) ; Disable alarms
+
+;; Font settings
+(set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 120)
+
+;; Theme
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ;("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initalize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t) ;; Installs packages that you use if they're not already installed
+
+;; Install ivy for completion
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1))
+
+(use-package counsel)
+
+(use-package doom-modeline
+  :ensure t
+  :custom ((doom-modeline-height 35))
+  :init (doom-modeline-mode 1))
