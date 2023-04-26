09801bc212d3df5c6920cb7094b818870c80b343
Nick Salesky <nicksalesky@gmail.com>
2023-04-26 08:51:38 -0400

    Commented out Emacs packages temporarily for Arch

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -500,99 +500,6 @@
 
 (setq disabled-command-function nil)
 
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j")
-   '("k" . "H-k")
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   ;; '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore)))
-
-(use-package meow
-  :config
-  (meow-setup)
-  (meow-global-mode 1)
-  :hook
-  (vterm-mode . (lambda () (meow-mode 0))))
-
 (electric-pair-mode 1)
 (setq electric-pair-inhibit-predicate
       (lambda (char)
@@ -931,7 +838,7 @@ are equal return nil."
   (add-hook 'org-present-mode-hook 'ns/org-present-begin)
   (add-hook 'org-present-mode-quit-hook 'ns/org-present-end))
 
-(use-package emacsql-sqlite-builtin)
+;; (use-package emacsql-sqlite-builtin)
 
 (use-package org-roam
   :diminish
@@ -950,7 +857,7 @@ are equal return nil."
   (org-roam-directory (file-truename "~/Documents/notes/"))
   (org-roam-file-extensions '("org" "md"))
   (org-roam-dailies-directory "logs")
-  (org-roam-database-connector 'sqlite-builtin)
+  ;; (org-roam-database-connector 'sqlite-builtin)
   (org-roam-capture-templates
    '(("d" "default" plain (file "~/Documents/notes/capture-templates/default.org")
       :target (file "${slug}.org")
@@ -1066,13 +973,6 @@ are equal return nil."
 ;;     (lsp-ui-sideline-show-hover t)
 ;;     (lsp-ui-doc-position 'bottom)
 ;;     (lsp-ui-doc-enable nil))
-
-(use-package treesit-auto
-  :straight (:type git :host github :repo "renzmann/treesit-auto")
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (global-treesit-auto-mode))
 
 ;; (require 'treesit)
 ;; (defun ns/tree-sitter-compile-grammar (destination &optional path)
@@ -1245,11 +1145,6 @@ are equal return nil."
   :after (org)
   :config
   (define-key org-mode-map (kbd "C-c C-r") verb-command-map))
-
-(use-package wakatime-mode
-  :diminish wakatime-mode
-  :config
-  (global-wakatime-mode))
 
 (use-package ws-butler
   :diminish ws-butler-mode
