d9f8b1239e13500e2c492f449f8db8d3f397b3bc
Nicholas Salesky <nicksalesky@gmail.com>
2023-05-18 21:27:45 -0700

    More Neovim remaps, upped Hyprland monitor refresh rate

--- nvim/.config/nvim/lua/nsalesky/remap.lua
+++ nvim/.config/nvim/lua/nsalesky/remap.lua
@@ -4,3 +4,21 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open Ex" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- keep the cursor in the midddle of the screen when jumping with C-u and C-d
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep the cursor in the middle of the screen for search terms
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- don't override register when pasting over a selection
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- switch to previous tmux session
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
