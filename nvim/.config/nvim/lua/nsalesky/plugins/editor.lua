bce57d0a9ae78b2629f87ad8e25fc86779e89661
Nicholas Salesky <nicksalesky@gmail.com>
2023-05-19 15:18:15 -0700

    Tmux config

--- nvim/.config/nvim/lua/nsalesky/plugins/editor.lua
+++ nvim/.config/nvim/lua/nsalesky/plugins/editor.lua
@@ -7,10 +7,10 @@ return {
 
 			vim.keymap.set("n", "<leader>a", mark.add_file)
 			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-			vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
-			vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-			vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-			vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+			--vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
+			--vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+			--vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+			--vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
 		end,
 	},
 	{
@@ -19,4 +19,8 @@ return {
 			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 		end,
 	},
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+    },
 }
