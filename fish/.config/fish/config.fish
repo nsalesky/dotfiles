dcf4c84399d7ac15d9a2a5b5018c9658781816af
Nicholas Salesky <nicksalesky@gmail.com>
2023-05-19 15:50:30 -0700

    Fixed fish in Tmux, Comment.nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,14 @@ set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
 
 # Use Vi keybindings
-# fish_vi_key_bindings
+
+fish_vi_key_bindings
+set -g fish_cursor_default block
+set -g fish_cursor_insert line
+set -g fish_cursor_replace_one underscore
+set -g fish_cursor_visual block
+
+# set -g fish_vi_force_cursor 1
 
 # Autojump
 set -gx AUTOJUMP_SOURCED 1
--- nvim/.config/nvim/lua/nsalesky/plugins/editor.lua
+++ nvim/.config/nvim/lua/nsalesky/plugins/editor.lua
@@ -7,10 +7,10 @@ return {
 
 			vim.keymap.set("n", "<leader>a", mark.add_file)
 			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-			--vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
-			--vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-			--vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-			--vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+			-- vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
+			-- vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+			-- vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+			-- vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
 		end,
 	},
 	{
@@ -22,5 +22,18 @@ return {
     {
         "christoomey/vim-tmux-navigator",
         lazy = false,
+    },
+    {
+        "numtostr/comment.nvim",
+        config = function()
+            require("Comment").setup({
+                toggler = {
+                    line = "<leader>/",
+                },
+                opleader = {
+                    line = "<leader>/",
+                },
+            })
+        end,
     },
 }
