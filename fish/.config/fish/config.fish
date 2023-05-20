9c59bf7eb8c6af740d3a4063275ebda714f686c5
Nicholas Salesky <nicksalesky@gmail.com>
2023-05-20 15:19:02 -0700

    Disabling Vi mode in Fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,11 +9,11 @@ set -gx VISUAL "nvim"
 
 # Use Vi keybindings
 
-fish_vi_key_bindings
-set -g fish_cursor_default block
-set -g fish_cursor_insert line
-set -g fish_cursor_replace_one underscore
-set -g fish_cursor_visual block
+# fish_vi_key_bindings
+# set -g fish_cursor_default block
+# set -g fish_cursor_insert line
+# set -g fish_cursor_replace_one underscore
+# set -g fish_cursor_visual block
 
 # set -g fish_vi_force_cursor 1
 
