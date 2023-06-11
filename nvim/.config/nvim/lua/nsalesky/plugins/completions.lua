d9dc8ba01be58d604895236e915c09f16db3b4de
Nicholas Salesky <nicksalesky@gmail.com>
2023-06-11 11:34:12 -0700

    Nvim friendly-snippets and set up Luasnip as a CMP source

--- nvim/.config/nvim/lua/nsalesky/plugins/completions.lua
+++ nvim/.config/nvim/lua/nsalesky/plugins/completions.lua
@@ -1,12 +1,11 @@
-
 return {
     {
         "hrsh7th/nvim-cmp",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+        },
         opts = function()
             return require("nsalesky.configs.cmp")
-        end,
-        config = function(_, opts)
-            require("cmp").setup(opts)
         end,
     },
     "hrsh7th/cmp-nvim-lsp",
@@ -14,6 +13,11 @@ return {
         "L3MON4D3/LuaSnip",
         version = "1",
         build = "make install_jsregexp", -- optional
+        dependencies = {
+            "rafamadriz/friendly-snippets", -- adds a bunch of preconfigured snippets
+        },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
     },
-    "onsails/lspkind.nvim",
 }
