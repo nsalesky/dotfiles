d90a15188a363f1fbaaefad8ebee8ba51969de5b
Nicholas Salesky <nicksalesky@gmail.com>
2023-07-02 08:49:27 -0700

    Disable Lazy reload notifications

--- nvim/.config/nvim/lua/nsalesky/lazy.lua
+++ nvim/.config/nvim/lua/nsalesky/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("nsalesky.plugins")
+require("lazy").setup("nsalesky.plugins", {
+    change_detection = {
+        notify = false,
+    },
+})
