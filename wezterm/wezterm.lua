9a158f609be83447c48dab489a0b318072f153cd
Nicholas Salesky <nicksalesky@gmail.com>
2023-07-14 21:29:43 -0700

    More wezterm config changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,36 +7,43 @@ wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Font settings
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 11
+config.font_size = 10
 config.line_height = 1.0
 config.color_scheme = "Catppuccin Macchiato"
 
 config.scrollback_lines = 5000
 
 -- Tab bar
--- config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
 
 -- Keybindings
 --config.disable_default_key_bindings = false
 
 config.keys = {
-    {
-        key = "E",
-        mods = "CTRL|SHIFT",
-        action = act.PromptInputLine({
-            description = "Enter new name for tab",
-            action = wezterm.action_callback(function(window, _, line)
-                if line then
-                    window:active_tab():set_title(line)
-                end
-            end),
-        }),
-    },
+	{
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 return config
