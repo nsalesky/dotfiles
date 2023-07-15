e65b58d7ca811b1e4a9dd8cef48f3e36c6fdc1e5
Nicholas Salesky <nicksalesky@gmail.com>
2023-07-14 22:09:42 -0700

    Lualine style changes

--- nvim/lua/nsalesky/configs/lualine.lua
+++ nvim/lua/nsalesky/configs/lualine.lua
@@ -15,11 +15,6 @@ local function get_current_filetype()
     return vim.api.nvim_buf_get_option(0, "filetype")
 end
 
--- Get the current buffer's type
-local function get_current_buftype()
-    return vim.api.nvim_buf_get_option(0, "buftype")
-end
-
 local function get_native_lsp()
     local buf_ft = get_current_filetype()
     local clients = vim.lsp.get_active_clients()
@@ -34,65 +29,22 @@ local function get_native_lsp()
     end
 end
 
--- Display the difference in commits between local and head
--- local Job = require("plenary.job")
--- local function get_git_compare()
---     -- Get the path of the current directory
---     local curr_dir = vim.api.nvim_buf_get_name(0):match("(.*" .. "/" .. ")")
---
---     -- Run job to fetch git info
---     local result = Job:new({
---             command = "git",
---             cwd = curr_dir,
---             args = { "rev-list", "--left-right", "--count", "HEAD...@{upstream}" },
---         })
---         :sync(100)[1]
---
---     -- Process the result
---     if type(result) ~= "string" then
---         return ""
---     end
---     local ok, ahead, behind = pcall(string.match, result, "(%d+)%s*(%d+)")
---     if not ok then
---         return ""
---     end
---
---     -- No file, so no git
---     if get_current_buftype() == "nofile" then
---         return ""
---     end
---
---     return "󰁅 " .. behind .. " 󰁝 " .. ahead
--- end
-
-local function telescope_text()
-    return "Telescope"
-end
-
 local telescope = {
     sections = {
         lualine_a = {
             {
-                "mode",
-                icon = { "" },
-                separator = { right = " ", left = "" },
+                function()
+                    return "Telescope"
+                end,
             },
         },
-        lualine_b = {
-            {
-                telescope_text,
-                icon = { "  " },
-                color = { fg = c.inner_pill_text, bg = c.inner_pill_bg },
-                separator = { right = "" },
-            },
-        },
+        lualine_b = {},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {
             {
                 "location",
-                icon = { "", align = "left" },
             },
         },
     },
@@ -103,8 +55,8 @@ return {
     options = {
         icons_enabled = true,
         theme = "auto",
-        section_separators = { left = " ", right = " " },
-        component_separators = { left = "", right = "" },
+        section_separators = " ",
+        component_separators = " ",
         disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -121,66 +73,38 @@ return {
     sections = {
         lualine_a = {
             {
-                "mode",
-                icon = { "" },
+                "filename",
+                file_status = true,
+                -- path = 4,
+                shorting_target = 40,
+                symbols = {
+                    modified = "", -- Text to show when the file is modified.
+                    readonly = "", -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = "No Name", -- Text to show for unnamed buffers.
+                    newfile = "", -- Text to show for newly created file before first write
+                },
                 color = { fg = c.outer_pill_text },
-                separator = { right = " ", left = "" },
             },
         },
         lualine_b = {
             {
-                "filetype",
-                colored = false,
-                icon_only = true,
+                "branch",
                 color = { fg = c.inner_pill_icon, bg = c.inner_pill_bg },
+                icon = { "", color = { fg = c.inner_pill_icon } },
             },
             {
-                "filename",
-                color = { fg = c.inner_pill_text, bg = c.inner_pill_bg },
-                separator = { right = " " },
-                padding = 0,
-
-                file_status = true,
-                path = 4,
-                shorting_target = 40,
-                symbols = {
-                    modified = "[+]",      -- Text to show when the file is modified.
-                    readonly = "[-]",      -- Text to show when the file is non-modifiable or readonly.
-                    unnamed = "[No Name]", -- Text to show for unnamed buffers.
-                    newfile = "[New]",     -- Text to show for newly created file before first write
+                "diff",
+                source = diff_source,
+                color = { fg = c.inner_pill_icon, bg = c.inner_pill_bg },
+                symbols = { added = "+", modified = "~", removed = "-" },
+                diff_color = {
+                    added = { fg = "#67f771" },
+                    modified = { fg = "#2fc0ed" },
+                    removed = { fg = "#f93b58" },
                 },
             },
         },
         lualine_c = {
-            {
-                "branch",
-                color = { fg = c.inner_fg, bg = c.inner_bg },
-                icon = { "", color = { fg = c.inner_pill_icon } },
-                separator = " ",
-                padding = 0,
-            },
-            -- {
-            --     get_git_compare,
-            --     color = { fg = c.inner_fg, bg = c.inner_bg },
-            --     icon = { " ", color = { fg = c.inner_pill_icon } },
-            --     separator = " ",
-            --     padding = 0,
-            -- },
-            {
-                "diff",
-                source = diff_source,
-                color = { fg = c.inner_fg, bg = c.inner_bg },
-                padding = 0,
-                icon = { " " },
-                symbols = { added = " ", modified = " ", removed = " " },
-                diff_color = {
-                    added = { fg = c.inner_pill_bg },
-                    modified = { fg = c.inner_pill_bg },
-                    removed = { fg = c.inner_pill_bg },
-                },
-            },
-        },
-        lualine_x = {
             {
                 "diagnostics",
                 sources = { "nvim_diagnostic" },
@@ -193,14 +117,23 @@ return {
                 --     hint = { fg = c.hint },
                 -- },
                 colored = true,
-                padding = 1,
+            },
+        },
+        lualine_x = {
+            {
+                "encoding",
+                color = { fg = c.inner_fg, bg = c.inner_bg },
+                padding = 0,
+            },
+            {
+                get_current_filetype,
+                color = { fg = c.inner_fg, bg = c.inner_bg },
+                padding = 0,
             },
         },
         lualine_y = {
             {
                 get_native_lsp,
-                padding = 2,
-                separator = " ",
                 color = { fg = c.inner_pill_text, bg = c.inner_pill_bg },
                 icon = { " ", color = { fg = c.inner_pill_icon } },
             },
@@ -209,13 +142,12 @@ return {
             {
                 "location",
                 color = { fg = c.outer_pill_text },
-                icon = { "", align = "left" },
+                padding = 1,
             },
             {
                 "progress",
                 color = { fg = c.outer_pill_text },
-                icon = { "", align = "left" },
-                separator = { right = "", left = "" },
+                padding = 1,
             },
         },
     },
--- nvim/lua/nsalesky/plugins/ui.lua
+++ nvim/lua/nsalesky/plugins/ui.lua
@@ -46,22 +46,22 @@ return {
             },
         },
     },
-    {
-        "romgrk/barbar.nvim",
-        dependencies = {
-            "lewis6991/gitsigns.nvim",     -- OPTIONAL: for git status
-            "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-        },
-        init = function()
-            vim.g.barbar_auto_setup = false
-        end,
-        opts = {
-            auto_hide = true,
-            sidebar_filetypes = {
-                NvimTree = true,
-                undotree = true,
-            },
-        },
-        version = "^1.0.0", -- optional: only update when a new 1.x version is released
-    },
+    -- {
+    --     "romgrk/barbar.nvim",
+    --     dependencies = {
+    --         "lewis6991/gitsigns.nvim",     -- OPTIONAL: for git status
+    --         "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    --     },
+    --     init = function()
+    --         vim.g.barbar_auto_setup = false
+    --     end,
+    --     opts = {
+    --         auto_hide = true,
+    --         sidebar_filetypes = {
+    --             NvimTree = true,
+    --             undotree = true,
+    --         },
+    --     },
+    --     version = "^1.0.0", -- optional: only update when a new 1.x version is released
+    -- },
 }
