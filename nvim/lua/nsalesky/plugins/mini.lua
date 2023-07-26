3705c98030b634a3253207d9688df3a0ae12db9d
Nicholas Salesky <nicksalesky@gmail.com>
2023-07-25 17:41:43 -0700

    Disable mini.files

--- nvim/lua/nsalesky/plugins/mini.lua
+++ nvim/lua/nsalesky/plugins/mini.lua
@@ -1,11 +1,11 @@
 return {
     "echasnovski/mini.nvim",
     version = false,
-    config = function()
-        require("mini.files").setup({
-            options = {
-                use_as_default_explorer = true,
-            },
-        })
-    end,
+    -- config = function()
+    --     require("mini.files").setup({
+    --         options = {
+    --             use_as_default_explorer = true,
+    --         },
+    --     })
+    -- end,
 }
