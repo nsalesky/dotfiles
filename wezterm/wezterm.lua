f98fa40c615670bf8499f2852ba3d8517e720b20
Nicholas Salesky <nicksalesky@gmail.com>
2023-09-16 10:41:57 -0400

    Tweaked wezterm settings to better fit in with Gnome GTK

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,9 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+local wayland_gnome = require("wayland_gnome")
+wayland_gnome.apply_to_config(config)
+
 -- Font settings
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 10
@@ -18,15 +21,22 @@ config.color_scheme = "Catppuccin Macchiato"
 
 config.scrollback_lines = 5000
 
+-- Window appearance
+config.window_decorations="INTEGRATED_BUTTONS|RESIZE"
+config.integrated_title_button_style = "Gnome"
+config.integrated_title_buttons = { "Hide", "Maximize", "Close" }
+
 -- Tab bar
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
+config.show_tabs_in_tab_bar = true
 
 config.window_padding = {
     left = 2,
-    right = 2,
-    top = 0,
+    right = 0,
+    top = 6,
     bottom = 0,
 }
+
 
 -- Keybindings
 --config.disable_default_key_bindings = false
