9138a4d23d6f362cd84b6465412bc84d1c6b5a18
Nicholas Salesky <nicksalesky@gmail.com>
2024-03-06 16:29:56 -0800

    Simplified nvim-cmp config

--- nvim/lua/nsalesky/configs/cmp.lua
+++ nvim/lua/nsalesky/configs/cmp.lua
@@ -28,12 +28,6 @@ local function format(entry, item)
   return item
 end
 
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
@@ -44,29 +38,6 @@ cmp.setup({
       c = cmp.mapping.close(),
     }),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- ["<Tab>"] = cmp.mapping(function(fallback)
-    --     if cmp.visible() then
-    --         cmp.select_next_item()
-    --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-    --         -- they way you will only jump inside the snippet region
-    --     elseif luasnip.expand_or_jumpable() then
-    --         luasnip.expand_or_jump()
-    --     elseif has_words_before() then
-    --         cmp.complete()
-    --     else
-    --         fallback()
-    --     end
-    -- end, { "i", "s" }),
-    --
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --     if cmp.visible() then
-    --         cmp.select_prev_item()
-    --     elseif luasnip.jumpable(-1) then
-    --         luasnip.jump(-1)
-    --     else
-    --         fallback()
-    --     end
-    -- end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
@@ -84,21 +55,6 @@ cmp.setup({
     fields = { "kind", "abbr" },
     format = format,
   },
-  -- window = {
-  --     completion = cmp.config.window.bordered({
-  --         -- winhighlight = 'Normal:Pmenu,FloatBorder:PmenuBorder,CursorLine:PmenuSel,Search:None',
-  --         scrollbar = true,
-  --         border = utils.border_chars_outer_thin,
-  --         col_offset = -1,
-  --         side_padding = 0,
-  --     }),
-  --     documentation = cmp.config.window.bordered {
-  --         -- winhighlight = 'Normal:Pmenu,FloatBorder:PmenuDocBorder,CursorLine:PmenuSel,Search:None',
-  --         scrollbar = true,
-  --         border = utils.border_chars_outer_thin,
-  --         side_padding = 1, -- Not working?
-  --     },
-  -- }
 })
 
 cmp.setup.filetype("gitcommit", {
