b0fcfaef5fc01cbd854a4676d7d87ab044d285e6
Nick Salesky <nicksalesky@gmail.com>
2024-06-08 17:37:23 -0700

    Added DAP configuration for C with gdb

--- nvim/lua/nsalesky/plugins/debugging.lua
+++ nvim/lua/nsalesky/plugins/debugging.lua
@@ -33,6 +33,27 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
+
+      -- Set up debug adapters
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "-i", "dap" },
+      }
+
+      -- Set up debug configurations
+      dap.configurations.c = {
+        {
+          name = "Launch",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false,
+        },
+      }
     end,
   },
   {
