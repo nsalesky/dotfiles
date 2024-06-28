999720a52372e028615cbe7c1e9569d03df8f772
Nicholas Salesky <nicksalesky@gmail.com>
2024-06-28 09:07:21 -0700

    Set up conform.nvim for code formatting

---
+++ nvim/lua/nsalesky/plugins/formatting.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+      -- Define formatters for each filetype
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+      },
+      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      -- Customize formatters
+      formatters = {
+
+      },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end
+  }
+}
